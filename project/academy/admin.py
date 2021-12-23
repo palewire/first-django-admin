945d26ca3dd22b3e45fe696a9077ed7314376291
Derek Willis <derek.willis@propublica.org>
2021-12-23 08:03:29 -0500

    Python 3/Django 4 version

--- project/academy/admin.py
+++ project/academy/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from academy.models import Invite
 
 class InviteAdmin(admin.ModelAdmin):
-    list_display = ("name", "branch", "gender", "date_of_birth", "race", "reporter")
-    list_filter = ("branch", "gender", "race", "reporter")
-    list_editable = ("reporter",)
-    search_fields = ("name",)
+  list_display = ("name", "branch", "gender", "date_of_birth", "race", "reporter",)
+  list_filter = ("branch", "gender", "race", "reporter",)
+  list_editable = ("reporter",)
+  search_fields = ("name",)
 
 admin.site.register(Invite, InviteAdmin)
--- project/academy/management/commands/dumpacademycsv.py
+++ project/academy/management/commands/dumpacademycsv.py
@@ -6,12 +6,12 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
-        print "Dumping CSV"
-        csv_path = os.path.join(settings.BASE_DIR, "dump.csv")
-        csv_file = open(csv_path, 'wb')
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['name', 'branch', 'gender', 'date_of_birth', 'race'])
-        for obj in Invite.objects.all():
-            row = [obj.name, obj.branch, obj.gender, obj.date_of_birth, obj.race]
-            csv_writer.writerow(row)
+  def handle(self, *args, **options):
+      print("Dumping CSV")
+      csv_path = os.path.join(settings.BASE_DIR, "dump.csv")
+      csv_file = open(csv_path, 'w')
+      csv_writer = csv.writer(csv_file)
+      csv_writer.writerow(['name', 'branch', 'gender', 'date_of_birth', 'race', 'notes', 'reporter'])
+      for obj in Invite.objects.all():
+          row = [obj.name, obj.branch, obj.gender, obj.date_of_birth, obj.race, obj.notes, obj.reporter]
+          csv_writer.writerow(row)
